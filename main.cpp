7a79b41b792e7989fffe86ed4a0829f6b6c0399e
DedAlexevich <kuznecovpetr502@yandex.ru>
2026-04-27 15:45:57 +0300

    Добавил остаток прошлой пары

--- main.cpp
+++ main.cpp
@@ -1,17 +1,17 @@
 #include <iostream>
 
-template< class T >
+template < class T >
 struct BiTree {
   T val;
-  BiTree< T >* left, * right, * parent;
+  BiTree< T >* left,* right,* parent;
 };
 
-template< class T >
+template < class T >
 struct BiTreeIt {
   BiTree< T >* curr;
 };
 
-template< class T>
+template < class T >
 BiTree< T >* minimum(BiTree< T >* root)
 {
   if (!root) {
@@ -23,7 +23,7 @@ BiTree< T >* minimum(BiTree< T >* root)
   return root;
 }
 
-template< class T>
+template < class T >
 BiTree< T >* maximum(BiTree< T >* root)
 {
   if (!root) {
@@ -35,21 +35,21 @@ BiTree< T >* maximum(BiTree< T >* root)
   return root;
 }
 
-template< class T >
-T& value(BiTreeIt<T> it)
+template < class T >
+T& value(BiTreeIt< T > it)
 {
   return it.curr->val;
 }
 
-template< class T >
-BiTreeIt< T > next(BiTreeIt<T> it)
+template < class T >
+BiTreeIt< T > next(BiTreeIt< T > it)
 {
   BiTree< T >* next = it.curr;
   if (next->rt) {
     next = next->rt;
     next = minimum(next);
   } else {
-    BiTree<T> parent = next->parent;
+    BiTree< T > parent = next->parent;
     while (parent && parent->lt != next) {
       next = parent;
       parent = next->parent;
@@ -59,15 +59,15 @@ BiTreeIt< T > next(BiTreeIt<T> it)
   return {next};
 }
 
-template< class T >
-BiTreeIt< T > prev(BiTreeIt<T> it)
+template < class T >
+BiTreeIt< T > prev(BiTreeIt< T > it)
 {
   BiTree< T >* next = it.curr;
   if (next->lt) {
     next = next->lt;
     next = maximum(next);
   } else {
-    BiTree<T> parent = next->parent;
+    BiTree< T > parent = next->parent;
     while (parent && parent->rt != next) {
       next = parent;
       parent = next->parent;
@@ -77,14 +77,14 @@ BiTreeIt< T > prev(BiTreeIt<T> it)
   return {next};
 }
 
-template< class T >
-bool hasNext(BiTreeIt<T> it)
+template < class T >
+bool hasNext(BiTreeIt< T > it)
 {
   return next(it).curr;
 }
 
-template< class T >
-bool hasPrev(BiTreeIt<T> it)
+template < class T >
+bool hasPrev(BiTreeIt< T > it)
 {
   return prev(it).curr;
 }
@@ -94,8 +94,8 @@ enum class Direction {
   parent
 };
 
-template< class T >
-BiTree<T> nextAssigned(BiTree< T >* root)
+template < class T >
+BiTree< T > nextAssigned(BiTree< T >* root)
 {
   if (!root) {
     return nullptr;
@@ -105,7 +105,7 @@ BiTree<T> nextAssigned(BiTree< T >* root)
     next = next->rt;
     next = minimum(next);
   } else {
-    BiTree<T> parent = next->parent;
+    BiTree< T > parent = next->parent;
     while (parent && parent->lt != next) {
       next = parent;
       parent = next->parent;
@@ -115,8 +115,8 @@ BiTree<T> nextAssigned(BiTree< T >* root)
   return next;
 }
 
-template< class T >
-std::pair< size_t, BiTree<T>* > fall_left(BiTree<T>* root)
+template < class T >
+std::pair< size_t, BiTree< T >* > fall_left(BiTree< T >* root)
 {
   size_t path = 0;
   while (root->left) {
@@ -126,14 +126,14 @@ std::pair< size_t, BiTree<T>* > fall_left(BiTree<T>* root)
   return {path, root};
 }
 
-template< class T >
-std::pair< size_t, BiTree<T>* > parent(BiTree<T>* root)
+template < class T >
+std::pair< size_t, BiTree< T >* > parent(BiTree< T >* root)
 {
   if (!root) {
     return {0, nullptr};
   }
   size_t path = 0;
-  BiTree<T> parent = root->parent;
+  BiTree< T > parent = root->parent;
   while (parent && parent->left != root) {
     root = parent;
     parent = root->parent;
@@ -142,8 +142,8 @@ std::pair< size_t, BiTree<T>* > parent(BiTree<T>* root)
   return {path, parent};
 }
 
-template< class T >
-std::tuple< Direction, size_t, BiTree< T >* > nextStruct(BiTree<T>* root)
+template < class T >
+std::tuple< Direction, size_t, BiTree< T >* > nextStruct(BiTree< T >* root)
 {
   if (root->rt) {
     auto res = fall_left(root->rt);
@@ -153,7 +153,7 @@ std::tuple< Direction, size_t, BiTree< T >* > nextStruct(BiTree<T>* root)
   return {Direction::parent, res.first, res.second};
 }
 
-template< class T >
+template < class T >
 bool isEqualStruct(BiTree< T >* lhs, BiTree< T >* rhs)
 {
   auto lhs_begin = fall_left(lhs);
@@ -163,19 +163,54 @@ bool isEqualStruct(BiTree< T >* lhs, BiTree< T >* rhs)
   }
   auto ln = nextStruct(lhs_begin.second);
   auto rn = nextStruct(rhs_begin.second);
-  while (std::get< 0 >(ln) == std::get< 0 >(rn) &&
-        std::get< 1 >(ln) == std::get< 1 >(rn) &&
-        std::get< 2 >(ln) && std::get< 2 >(rn)) {
+  while (std::get< 0 >(ln) == std::get< 0 >(rn) && std::get< 1 >(ln) == std::get< 1 >(rn) && std::get< 2 >(ln) &&
+    std::get< 2 >(rn)) {
     ln = nextStruct(std::get< 2 >(ln));
     rn = nextStruct(std::get< 2 >(rn));
   }
   return ln == rn;
 }
 
+template < class T >
+bool includedStructRoot(BiTree< T >* lhs_root, BiTree< T >* pattern)
+{
+  auto next_pattern = nextStruct(pattern);
+  std::pair< size_t, BiTree< T >* > next_lhs;
+  if (std::get< 0 >(next_pattern) == Direction::fall_left) {
+    next_lhs = fall_left(lhs_root);
+  } else {
+    next_lhs = parent(lhs_root);
+  }
+  while (std::get< 1 >(next_pattern) == next_lhs.first && std::get< 2 >(next_pattern) && next_lhs.second) {
+    next_pattern = nextStruct(std::get< 2 >(next_pattern));
+    if (std::get< 0 >(next_pattern) == Direction::fall_left) {
+      if (next_lhs.second->right) {
+        next_lhs = fall_left(next_lhs.second->right);
+      } else {
+        return false;
+      }
+    } else {
+      next_lhs = parent(next_lhs.second);
+    }
+  }
+  return std::get< 1 >(next_pattern) == next_lhs.first && !std::get< 2 >(next_pattern);
+}
 
+template < class T >
+bool includedStruct(BiTree< T >* lhs, BiTree< T >* pattern)
+{
+  pattern = fall_left(pattern).second;
+  lhs = fall_left(lhs).second;
+  while (lhs) {
+    if (includedStructRoot(lhs, pattern)) {
+      return true;
+    }
+    lhs = std::get< 2 >(nextStruct(lhs));
+  }
+  return false;
+}
 
 int main()
 {
-
   return 0;
 }
